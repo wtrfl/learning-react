371186d6c1a7e90f9f23ed3fde62790b3c30b548
waterfall <78034265+wtrfl@users.noreply.github.com>
2025-02-04 18:51:48 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+export default function App() {
+    return (
+        <div className="w-full h-full flex flex-col items-center justify-center">
+            <img src="fun_drawing.png" alt="" />
+            <span className="text-xl mb-2">My React Learning Adventure!</span>
+            <span className="w-120 text-center text-gray-600">Hello, I am @wtrfl, and I am working to bridge the gaps in my React knowledge. Hold me accountable and follow along here or on the GitHub repository to see my progress and my projects. Updating list of projects soon to come!</span>
+        </div>
+    )
+}
