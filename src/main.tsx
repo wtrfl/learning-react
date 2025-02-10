735104f933c3110d65a11c09aa250a742e19b478
waterfall <78034265+wtrfl@users.noreply.github.com>
2025-02-10 20:40:23 +0100

    test: reset

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import Sums from './useMemo/Sums.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Sums />
+    <App />
   </StrictMode>,
 )
