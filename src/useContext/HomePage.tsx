98be92496d589f070007d0039adce030a75b078b
waterfall <78034265+wtrfl@users.noreply.github.com>
2025-02-14 12:04:57 +0100

    minor ui updates

--- src/useContext/HomePage.tsx
+++ src/useContext/HomePage.tsx
@@ -6,24 +6,24 @@ export default function HomePage() {
 
     return (
         <div className="w-full h-full m-0 flex flex-col">
-            <div className="w-full flex space-between">
-                <div className="flex justify-center">
+            <div className="w-full flex justify-between">
+                <div className="flex items-center">
                     <span className="font-semibold text-lg">WARZYLAND</span>
                     {["Home", "About", "Contact"].map(item => (
                         <span className="px-2 py-4 hover:bg-gray-300 cursor-pointer">{item}</span>
                     ))}
                 </div>
-                <div className="flex justify-center">
-                    <button className="px-2 py-2 bg-gray-300 rounded-md cursor-pointer hover:bg-gray-500" onClick={() => setTheme(!theme)}>{theme ? 'Light' : 'Dark'} Mode</button>
-                    <button className="px-2 py-2 bg-blue-500 rounded-md cursor-pointer hover:bg-blue-700">Log in</button>
+                <div className="flex items-center">
+                    <button className="px-2 py-1 bg-gray-200 rounded-md cursor-pointer hover:bg-gray-500" onClick={() => setTheme(!theme)}>{theme ? 'Light' : 'Dark'} Mode</button>
+                    <button className="px-2 py-1 bg-blue-500 rounded-md cursor-pointer hover:bg-blue-700">Log in</button>
                 </div>
             </div>
             <div className="flex flex-grow justify-center items-center">
                 <div className="flex flex-col">
                     <span className="text-2xl">We are building the city of dreams.</span>
-                    <span className="width-[300px]">
-                        Third spaces, equal opportunity social services, robust mass transit systems, and all daily essentials, within a gorgeous 7 minute* walk.
-                        <div className="inline-block bg-blue-500 p-1 text-white font-semibold">NEW</div> We are now offering subsidies to land-owners who invest early.
+                    <span className="w-[300px] text-gray-800 font-light">
+                        Third spaces, equal opportunity social services, robust mass transit systems, and daily essentials, all within a gorgeous 7 minute* walk.
+                        <br /><br /><div className="inline-block bg-blue-500 px-1 text-white font-semibold">NEW</div> We are now offering subsidies to land-owners who invest early.
                     </span>
                     <div className="flex gap-2">
                         <button>Join Us</button>
